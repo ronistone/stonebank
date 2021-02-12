4824d7b1907ddb1cc13bd143d09890d5b4642da9
Ronistone Junior <ronistonejunior@gmail.com>
2021-02-11 22:05:52 -0300

    adding transaction consult

--- application/src/main/kotlin/br/com/ronistone/stonebank/application/controller/TransactionController.kt
+++ application/src/main/kotlin/br/com/ronistone/stonebank/application/controller/TransactionController.kt
@@ -4,6 +4,8 @@ import br.com.ronistone.stonebank.domain.TransactionDTO
 import br.com.ronistone.stonebank.service.TransactionService
 import br.com.ronistone.stonebank.service.commons.toDTO
 import br.com.ronistone.stonebank.service.commons.toEntity
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -20,6 +22,16 @@ class TransactionController(
     @PostMapping(path = ["/customer/{id}/transfer"])
     fun transfer(@PathVariable id: UUID, @RequestBody transactionDTO: TransactionDTO): TransactionDTO {
         return transactionService.createTransfer(id, transactionDTO.toEntity()).toDTO()
+    }
+
+    @GetMapping(path = ["/{id}"])
+    fun findTransaction(@PathVariable("id") id: UUID): ResponseEntity<TransactionDTO> {
+        val transaction = transactionService.getTransaction(id)
+
+        if(transaction.isEmpty) {
+            return ResponseEntity.notFound().build()
+        }
+        return ResponseEntity.ok(transaction.get().toDTO())
     }
 
 }
