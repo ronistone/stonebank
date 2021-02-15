aa87d7d027ba4eba3d6cdf814a3309e86236d696
Ronistone Junior <ronistonejunior@gmail.com>
2021-02-15 15:54:20 -0300

    separating camunda processor in another microservice

--- processor/src/main/kotlin/br/com/ronistone/stonebank/processor/ProcessorApplication.kt
+++ processor/src/main/kotlin/br/com/ronistone/stonebank/processor/ProcessorApplication.kt
@@ -1,18 +1,18 @@
-package br.com.ronistone.stonebank.application
+package br.com.ronistone.stonebank.processor
 
+import org.camunda.bpm.spring.boot.starter.SpringBootProcessApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = ["br.com.ronistone.stonebank.repository"])
 @ComponentScan(basePackages = ["br.com.ronistone.stonebank", "br.com.ronistone.stonebank.repository"])
 @EntityScan(basePackages = ["br.com.ronistone.stonebank"])
-class Application
+class ProcessorApplication: SpringBootProcessApplication()
 
 fun main(args: Array<String>) {
-    runApplication<Application>(*args)
+    runApplication<ProcessorApplication>(*args)
 }
